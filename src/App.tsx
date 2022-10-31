0cf058449465f9a31d3cafb7352eebce531f09b1
_Allan <allankipchumba76@gmail.com>
2022-10-31 15:34:12 +0300

    bootstraped tsx app with typescript template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./app.scss";
 
-function App() {
+export const App = () => {
+  const [number, setNumber] = useState(5);
+
+  const changeNumber = () => setNumber(10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>People invited to my party</h1>
     </div>
   );
-}
-
-export default App;
+};
